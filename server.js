e16047005d74bfb5e8d901c51635af5b03e27697
Kerja9999999 <106123557+Kerja9999999@users.noreply.github.com>
2026-07-15 20:32:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 require("dotenv").config();
 
 const express = require("express");
+const TelegramBot = require("node-telegram-bot-api");
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
+const bot = new TelegramBot(process.env.BOT_TOKEN, {
+  polling: true,
+});
+
 app.get("/", (req, res) => {
-  res.send("✅ Semja Bot Server is running");
+  res.send("✅ Semja Bot работает");
+});
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "🏠 Добро пожаловать в Семейный бюджет!");
 });
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server started on port ${PORT}`);
+  console.log(`🚀 Server started on ${PORT}`);
 });
