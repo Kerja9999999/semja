339835a62d6d820d5131fcec2e94a8f455035628
Kerja9999999 <106123557+Kerja9999999@users.noreply.github.com>
2026-07-15 20:51:30 +0300

    Update storage.js

--- services/storage.js
+++ services/storage.js
@@ -1,1 +1,18 @@
+const fs = require("fs");
 
+function load(file) {
+    try {
+        return JSON.parse(fs.readFileSync(file, "utf8"));
+    } catch {
+        return [];
+    }
+}
+
+function save(file, data) {
+    fs.writeFileSync(file, JSON.stringify(data, null, 2));
+}
+
+module.exports = {
+    load,
+    save
+};
