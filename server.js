854b7acd208365c48b07aa89eaa51f378d8f19a5
Kerja9999999 <106123557+Kerja9999999@users.noreply.github.com>
2026-07-15 20:26:21 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("✅ Semja Bot Server is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server started on port ${PORT}`);
+});
