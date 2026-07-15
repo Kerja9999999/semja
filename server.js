892dd4e2620687b4a9cc87cff13c1359484424bb
Kerja9999999 <106123557+Kerja9999999@users.noreply.github.com>
2026-07-15 20:46:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,44 +1,23 @@
 require("dotenv").config();
-const userStates = require("./bot/states");
-const expenseCategories = require("./bot/categories");
-const menu = require("./bot/menu");
+
 const express = require("express");
 const TelegramBot = require("node-telegram-bot-api");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, {
-  polling: true,
+    polling: true
 });
+
+require("./bot/handlers/start")(bot);
+require("./bot/handlers/expenses")(bot);
 
 app.get("/", (req, res) => {
-  res.send("✅ Semja Bot работает");
+    res.send("Semja Bot работает");
 });
 
-bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(
-    msg.chat.id,
-    "🏠 Семейный бюджет\n\nВыберите действие:",
-    menu
-  );
-});
-bot.on("message", (msg) => {
+const PORT = process.env.PORT || 3000;
 
-    if (msg.text === "💰 Добавить расход") {
-
-        userStates[msg.chat.id] = {
-            step: "category"
-        };
-
-        return bot.sendMessage(
-            msg.chat.id,
-            "Выберите категорию расхода:",
-            expenseCategories
-        );
-    }
-
-});
 app.listen(PORT, () => {
-  console.log(`🚀 Server started on ${PORT}`);
+    console.log("Server started");
 });
