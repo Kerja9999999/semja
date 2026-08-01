b9fac605a504fedffea56289c3796c34768e0618
Kerja9999999 <106123557+Kerja9999999@users.noreply.github.com>
2026-08-02 01:24:06 +0300

    Update server.js

--- gate-system/server.js
+++ gate-system/server.js
@@ -1,1 +1,45 @@
 
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(express.json());
+
+let requestWaiting = false;
+
+// Гость нажал кнопку
+app.post("/request-open", (req, res) => {
+    requestWaiting = true;
+
+    console.log("Новый запрос на открытие ворот");
+
+    res.json({
+        success: true,
+        message: "Запрос отправлен"
+    });
+});
+
+// ESP32 будет спрашивать сервер
+app.get("/status", (req, res) => {
+
+    if (requestWaiting) {
+
+        requestWaiting = false;
+
+        return res.json({
+            open: true
+        });
+
+    }
+
+    res.json({
+        open: false
+    });
+
+});
+
+app.listen(PORT, () => {
+    console.log("Server started on port " + PORT);
+});
